54b48bf61db3f84a6ba1ce5b9ae1d4c54cbe5641
Kyle Alan Hale <khale@ancestry.com>
2015-02-23 14:58:55 -0800

    Initial Commit

---
+++ OpenCViOS/OpenCViOS.h
@@ -0,0 +1,19 @@
+//
+//  OpenCViOS.h
+//  OpenCViOS
+//
+//  Created by Kyle Alan Hale on 2/23/15.
+//  Copyright (c) 2015 Kyle Alan Hale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpenCViOS.
+FOUNDATION_EXPORT double OpenCViOSVersionNumber;
+
+//! Project version string for OpenCViOS.
+FOUNDATION_EXPORT const unsigned char OpenCViOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenCViOS/PublicHeader.h>
+
+
